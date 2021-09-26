e521817bde87f44497d5673e05d03f124fa13f4a
Abdullah Javaid Chaudhry <aqhr050@gmail.com>
2021-09-26 16:38:06 -0400

    alternateApproachRemoved

--- trainHMM.py
+++ trainHMM.py
@@ -9,7 +9,6 @@ def main():
     #dictionary will contain a dictionary entry for POS and said entry will contain details key-value pairs of each word having that POS and its count
     absoluteLikelihood = {}
     #dictionary will contain a dictionary entry for POS X and said entry will contain details key-value pairs of each POS Y that is subsequent to POS Y and the count of Y will be noted
-    tags = ["Begin_Sent"]
     absoluteTransition = {}
     with open(TRAINING_FILE, "r") as trainingFile:
         count = 0
@@ -19,14 +18,11 @@ def main():
         for line in trainingFile:
             if (re.match(r"^\s+$", line)):
                 sentenceEnd = True
-                tags.append("End_Sent")
-                tags.append("Begin_Sent")
                 word = "End_sent"
                 presentTag = "End_sent"
             else:
                 line = line.strip()
                 word, presentTag = line.split()
-                tags.append(presentTag)
             #creating an entry into absoluteLikeLihood, as in likelihood entry denominated in frequency
             #will check if a previous entry exists for the given presentTag, if not one is created, an entry for End_Sent will also be created as it stands
             if presentTag not in absoluteLikelihood:
@@ -61,21 +57,6 @@ def main():
         for word, count in WordDict.items():
             print(f"\t{word:<15} | {count}", end=" + ")
         print()
-
-    #An alternate approach
-    altCount = {}
-    bigramsList = nltk.bigrams(tags)
-    for bigram in bigramsList:
-        prevTag, currTag = bigram[0], bigram[1]
-        if prevTag not in altCount:
-            altCount[prevTag] = {}
-        altCount[prevTag][currTag] = altCount[prevTag].get(currTag, 0) + 1
-    for POS, WordDict in altCount.items():
-        print(POS)
-        for word, count in WordDict.items():
-            print(f"\t{word:<15} | {count}", end=" + ")
-        print()
-    
 
 
 if __name__ == "__main__":
